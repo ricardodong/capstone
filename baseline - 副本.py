b2c83d17dddb4ec47a02f75abd3bf50de0b24659
ricardodong <shutong@ualberta.ca>
2020-02-03 23:57:38 -0700

    add new yellow

--- baseline - 副本.py
+++ baseline - 副本.py
@@ -378,6 +378,65 @@ def add_new_row(current_tree_bottom, total_tree):
         return add_new_row(current_tree_bottom, total_tree) # choose a new block and try again if no options available
 
 
+def add_yellow_bird_new_row(current_tree_bottom, total_tree):
+    groupings = generate_subsets(
+        current_tree_bottom)  # generate possible groupings of bottom row objects, this is the previous row
+    choosen_item = 3  # choosen block for new row
+    center_groupings = []  # collection of viable groupings with new block at center
+    edge_groupings = []  # collection of viable groupings with new block at edges
+    both_groupings = []  # collection of viable groupings with new block at both center and edges
+
+    # check if new block is viable for each grouping in each position
+    for grouping in groupings:
+        if check_center(grouping, choosen_item, current_tree_bottom):  # check if center viable
+            center_groupings.append(grouping)
+        if check_edge(grouping, choosen_item, current_tree_bottom):  # check if edges viable
+            edge_groupings.append(grouping)
+        if check_both(grouping, choosen_item, current_tree_bottom):  # check if both center and edges viable
+            both_groupings.append(grouping)
+
+    # randomly choose a configuration (grouping/placement) from the viable options
+    total_options = len(center_groupings) + len(edge_groupings) + len(both_groupings)  # total number of options
+    if total_options > 0:
+        option = randint(1, total_options)
+        if option > len(center_groupings) + len(edge_groupings):
+            selected_grouping = both_groupings[option - (len(center_groupings) + len(edge_groupings) + 1)]
+            placement_method = 2
+        elif option > len(center_groupings):
+            selected_grouping = edge_groupings[option - (len(center_groupings) + 1)]
+            placement_method = 1
+        else:
+            selected_grouping = center_groupings[option - 1]
+            placement_method = 0
+
+        # construct the new bottom row for structure using selected block/configuration
+        new_bottom = []
+        for subset in selected_grouping:
+            if placement_method == 0:
+                new_bottom.append([choosen_item, find_subset_center(subset)])
+            if placement_method == 1:
+                new_bottom.append([choosen_item, find_subset_edges(subset)[0]])
+                new_bottom.append([choosen_item, find_subset_edges(subset)[1]])
+            if placement_method == 2:
+                new_bottom.append([choosen_item, find_subset_edges(subset)[0]])
+                new_bottom.append([choosen_item, find_subset_center(subset)])
+                new_bottom.append([choosen_item, find_subset_edges(subset)[1]])
+
+        for i in new_bottom:
+            i[1] = round(i[1], 10)  # round all values to prevent floating point inaccuracy from causing errors
+
+        if (new_bottom[0][1] + 0.9) < new_bottom[1][1]:
+            new_bottom.append([choosen_item, round(new_bottom[0][1] + 0.45, 10)])
+            new_bottom = sorted(new_bottom, key=lambda block: block[1])
+            print(new_bottom)
+
+        current_tree_bottom = new_bottom
+        total_tree.append(current_tree_bottom)  # add new bottom row to the structure
+
+        return total_tree, current_tree_bottom  # return the new structure
+    else:
+        print("impossible")
+        return False, False
 
 
 # creates the peaks (first row) of the structure
